ff63f526e4ed5f313c7424c17490822d6fa6e513
peekaboo2803 <kirtanskh@gmail.com>
2024-11-06 15:22:04 -0500

    Now goodreads is used and added some more features like aurthor name, published date.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # StorySynth
 This software is perfect for book lovers of any genre. It provides detailed summaries, character insights, and theme analyses, helping readers explore novels more deeply. Whether you're a casual reader or a literature enthusiast, this tool enhances your understanding and enjoyment of books.
-
+GoodReads is used to fetch details.
 #### In progress*
 
 ### Basic model (base)
--- main.py
+++ main.py
@@ -7,15 +7,23 @@ import openai
 
 def book_name(): 
     try:    #if the book name matches to any book in the website this block will get run
+        print("This results are from website Goodreads")
         url_Book_Name = (str(Book_Name.replace(' ','+'))) #for making url
-        url = ("https://openlibrary.org/search?q="+url_Book_Name+"&mode=everything") #creating url
+        url = ("https://www.goodreads.com/search?utf8=✓&q="+url_Book_Name+"&search_type=books") #creating url
+        #print(url)
         response = requests.get(url, headers=headers, cookies=cookies)
         soup = BeautifulSoup(response.content, 'lxml')
-        book = soup.find('h3', class_ = 'booktitle').text.replace('\n','')
-        publish_date_span = soup.find('span', class_ = "resultDetails") #parent
-        publish_date = publish_date_span.find('span').text.replace('\n','').replace(' ','',14) #child
-        print(book) 
-        print(publish_date)
+        book = soup.find('a', class_ = 'bookTitle').text.replace('\n','')
+        author_name = soup.find('a', class_ = "authorName").text
+        rating_of_book = soup.find('span', class_ = "minirating").text.replace('—',"which is average of")
+        publish_date = soup.find('span', class_="greyText smallText uitext")#replace('\n','').replace(' ','',14) #parent
+        publish_date.span.clear()
+        publish_date.a.clear()
+        publish_date_text = (publish_date).text.replace('\n','').replace('—','').replace("                              published","Pulished in:- ").replace('               ',' ')
+        print("Name:- "+book) 
+        print("Author's Name:- "+author_name)
+        print("Rating of the book out of 5:- "+rating_of_book)
+        print(publish_date_text)
 
     except AttributeError: #if there is no book of the same name of book on website the program will start again using this block of code
         book_info_needed()
@@ -25,19 +33,24 @@ secret_key = openai.api_key = str(os.getenv("OPENAI_SECRET_KEY"))
 
 
 def Summary(x): #in this function we will send promt to chatgpt and return the summary made by chatgpt.
-    Chat_For_Summary = openai.chat.completions.create(
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "user",
-                "content": ("summary of book " + x),
+                "content": (" give me summary of book " + x),
             }
-        ],
-        model="gpt-3.5-turbo",)
+        ]
+
+    )
+    answer = response['choices'][0]['message']['content']
+    print(answer)
     
 
 def book_info_needed():
     global Book_Name, options
     Book_Name = input("Enter the book name: ", ) #Name of the book you want to know about
+    Book_Name = Book_Name.lower()
     options = input("What output do desire?\n1.Details\n2.Summary\n=>") #what do you want to know about
     if options == "1" or "Details" or "details" :
         book_name()
